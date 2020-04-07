af166d0adf279abaa2339d406601d949f0e4efe5
Joël Matelli <joel.matelli@tellijo.fr>
2020-04-07 02:03:18 +0200

    feat: remove CRA template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+        TimeBomb
+    </header>
+  </div>
+);
+
+export default App;
