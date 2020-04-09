b94d70463334f0473c30e46d242c982a297737f7
Joël Matelli <joel.matelli@tellijo.fr>
2020-04-09 23:24:52 +0200

    chore: add eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
 
 const App = () => (
   <div className="App">
-    <header className="App-header">
-        TimeBomb
-    </header>
+    <header className="App-header">TimeBomb</header>
   </div>
 );
 
