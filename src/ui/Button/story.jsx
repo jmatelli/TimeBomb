1dc8e455511aa61e811418bf0a5c5d04fad407f5
Joël Matelli <joel.matelli@tellijo.fr>
2020-04-12 19:09:21 +0200

    feat: handle button size

--- src/ui/Button/story.jsx
+++ src/ui/Button/story.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { action } from '@storybook/addon-actions';
+import styled from 'styled-components';
+
 import Button from './index';
+
+const Margin = styled.div`
+  margin: 10px;
+`;
 
 export default {
   component: Button,
@@ -8,43 +14,211 @@ export default {
 };
 
 export const primary = () => (
-  <Button type="primary" onClick={action('clicked')}>
-    Hello Button
-  </Button>
+  <Fragment>
+    <Margin>
+      <Button type="primary" size="small" onClick={action('clicked')}>
+        hello button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="primary" onClick={action('clicked')}>
+        hello button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="primary" size="large" onClick={action('clicked')}>
+        hello button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="primary" size="small" loading onClick={action('clicked')}>
+        Hello Button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="primary" loading onClick={action('clicked')}>
+        Hello Button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="primary" size="large" loading onClick={action('clicked')}>
+        Hello Button
+      </Button>
+    </Margin>
+  </Fragment>
 );
 
 export const secondary = () => (
-  <Button type="secondary" onClick={action('clicked')}>
-    Hello Button
-  </Button>
+  <Fragment>
+    <Margin>
+      <Button type="secondary" size="small" onClick={action('clicked')}>
+        hello button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="secondary" onClick={action('clicked')}>
+        hello button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="secondary" size="large" onClick={action('clicked')}>
+        hello button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="secondary" size="small" loading onClick={action('clicked')}>
+        Hello Button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="secondary" loading onClick={action('clicked')}>
+        Hello Button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="secondary" size="large" loading onClick={action('clicked')}>
+        Hello Button
+      </Button>
+    </Margin>
+  </Fragment>
 );
 
 export const success = () => (
-  <Button type="success" onClick={action('clicked')}>
-    Hello Button
-  </Button>
+  <Fragment>
+    <Margin>
+      <Button type="success" size="small" onClick={action('clicked')}>
+        hello button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="success" onClick={action('clicked')}>
+        hello button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="success" size="large" onClick={action('clicked')}>
+        hello button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="success" size="small" loading onClick={action('clicked')}>
+        Hello Button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="success" loading onClick={action('clicked')}>
+        Hello Button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="success" size="large" loading onClick={action('clicked')}>
+        Hello Button
+      </Button>
+    </Margin>
+  </Fragment>
 );
 
 export const info = () => (
-  <Button type="info" onClick={action('clicked')}>
-    Hello Button
-  </Button>
+  <Fragment>
+    <Margin>
+      <Button type="info" size="small" onClick={action('clicked')}>
+        hello button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="info" onClick={action('clicked')}>
+        hello button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="info" size="large" onClick={action('clicked')}>
+        hello button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="info" size="small" loading onClick={action('clicked')}>
+        Hello Button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="info" loading onClick={action('clicked')}>
+        Hello Button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="info" size="large" loading onClick={action('clicked')}>
+        Hello Button
+      </Button>
+    </Margin>
+  </Fragment>
 );
 
 export const warning = () => (
-  <Button type="warning" onClick={action('clicked')}>
-    Hello Button
-  </Button>
+  <Fragment>
+    <Margin>
+      <Button type="warning" size="small" onClick={action('clicked')}>
+        hello button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="warning" onClick={action('clicked')}>
+        hello button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="warning" size="large" onClick={action('clicked')}>
+        hello button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="warning" size="small" loading onClick={action('clicked')}>
+        Hello Button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="warning" loading onClick={action('clicked')}>
+        Hello Button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="warning" size="large" loading onClick={action('clicked')}>
+        Hello Button
+      </Button>
+    </Margin>
+  </Fragment>
 );
 
 export const danger = () => (
-  <Button type="danger" onClick={action('clicked')}>
-    Hello Button
-  </Button>
-);
-
-export const loading = () => (
-  <Button type="primary" loading onClick={action('clicked')}>
-    Hello Button
-  </Button>
+  <Fragment>
+    <Margin>
+      <Button type="danger" size="small" onClick={action('clicked')}>
+        hello button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="danger" onClick={action('clicked')}>
+        hello button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="danger" size="large" onClick={action('clicked')}>
+        hello button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="danger" size="small" loading onClick={action('clicked')}>
+        Hello Button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="danger" loading onClick={action('clicked')}>
+        Hello Button
+      </Button>
+    </Margin>
+    <Margin>
+      <Button type="danger" size="large" loading onClick={action('clicked')}>
+        Hello Button
+      </Button>
+    </Margin>
+  </Fragment>
 );
