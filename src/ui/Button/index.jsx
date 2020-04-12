a41825fe9fe9aa5d680331dd556107c7ef739c6a
Joël Matelli <joel.matelli@tellijo.fr>
2020-04-12 19:09:21 +0200

    feat: handle button size

--- src/ui/Button/index.jsx
+++ src/ui/Button/index.jsx
@@ -10,13 +10,12 @@ const ViewButton = styled.button`
   border-radius: 3px;
   cursor: pointer;
   color: white;
+  font-weight: bold;
   background: ${({ type }) => (type ? colors[type].base : colors.default.base)};
-  font-size: 16px;
   box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
   display: flex;
   flex-direction: row;
   padding: 0;
-  margin: 10px;
   position: relative;
   overflow: hidden;
   &:hover {
@@ -27,29 +26,34 @@ const ViewButton = styled.button`
 const ViewIcon = styled.span`
   display: flex;
   height: 100%;
-  padding: 11px;
+  padding: ${({ size }) => (size === 'large' ? `11px` : '8px')};
   margin: 0;
   background: ${({ type }) => colors[type].light};
+  font-size: ${({ size }) => (size === 'small' ? '14px' : '16px')};
 `;
 
 const ViewChildren = styled.span`
   display: flex;
-  padding: ${({ loading }) => (loading ? `10px 20px 10px 15px` : `10px 20px`)};
+  padding: ${({ size }) => (size === 'large' ? `10px 20px` : `7px 15px`)};
+  font-size: ${({ size }) => (size === 'small' ? '14px' : '16px')};
 `;
 
-const Button = ({ type, loading, children }) => (
+const Button = ({ type = 'primary', size = 'normal', loading = false, children }) => (
   <ViewButton type={type}>
     {loading && (
-      <ViewIcon type={type}>
-        <FontAwesomeIcon icon={faSpinnerThird} spin swapOpacity />
+      <ViewIcon type={type} size={size}>
+        <FontAwesomeIcon icon={faSpinnerThird} spin />
       </ViewIcon>
     )}
-    <ViewChildren loading={loading}>{children}</ViewChildren>
+    <ViewChildren loading={loading} size={size}>
+      {children}
+    </ViewChildren>
   </ViewButton>
 );
 
 Button.propTypes = {
   type: t.string,
+  size: t.oneOf(['small', 'normal', 'large']),
   loading: t.bool,
   children: t.node,
 };
