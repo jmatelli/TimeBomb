d5f47c4fb6eec8652c6f094ab71fdc508697dd94
Joël Matelli <joel.matelli@tellijo.fr>
2020-04-12 17:53:16 +0200

    feat: add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Header, GlobalStyle } from './ui';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 const App = () => (
-  <Fragment>
+  <Router>
     <GlobalStyle />
     <Header />
-  </Fragment>
+    <Switch>
+      <Route path="/" exact component={Home} />
+    </Switch>
+  </Router>
 );
 
 export default App;
