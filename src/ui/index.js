b12b20715dfcb446e1d2149c64371803e2b35787
Joël Matelli <joel.matelli@tellijo.fr>
2020-04-13 21:11:08 +0200

    refactor(ui): update input

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,5 +1,6 @@
 export { default as Button } from './Button';
 export { default as Header } from './Header';
+export { default as Input } from './Input';
 
 export { default as GlobalStyle } from './global';
 export { default as colors } from './colors';
