cfbcaae9aba8e7adf8a051aedbc4fc6690d93016
Joël Matelli <joel.matelli@tellijo.fr>
2020-04-15 23:24:12 +0200

    feat(home): trigger create room in firebase after creating player

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,5 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp();
-
-const db = admin.firestore();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.createRoom = functions.firestore.document('players/{playerId}').onCreate((snap) => {
-  const { roomName, nbPlayers } = snap.data();
-  db.collection('rooms').add({
-    roomName,
-    nbPlayers,
-    players: [snap.id],
-  });
-});
