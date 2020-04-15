92539c5b0e63247f10297d798a1672e71041ed23
Joël Matelli <joel.matelli@tellijo.fr>
2020-04-16 00:28:03 +0200

    feat(home): redirect user to room page after creating game

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Header, GlobalStyle } from './ui';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Room from './pages/Room';
 
 const App = () => (
   <Router>
@@ -9,6 +10,7 @@ const App = () => (
     <Header />
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/rooms/:roomName" exact component={Room} />
     </Switch>
   </Router>
 );
---
+++ src/pages/Room/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ViewTitle = styled.h2`
+  font-size: 18px;
+`;
+
+const Room = () => <ViewTitle>Room</ViewTitle>;
+
+export default Room;
